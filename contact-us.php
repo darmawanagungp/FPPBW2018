1563761be6ecf44aa0d5ab4e397467506e65bed2
darmawanagungp <darmawannap@gmail.com>
2018-05-17 08:40:00 +0700

    List Mobil

--- contact-us.php
+++ contact-us.php
@@ -50,15 +50,6 @@ $error="Something went wrong. Please try again";
 <!--FontAwesome Font Style -->
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
 
-<!-- SWITCHER -->
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
-
 <!-- Fav and touch icons -->
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
@@ -136,7 +127,7 @@ $error="Something went wrong. Please try again";
               <textarea class="form-control white_bg" name="message" rows="4" required></textarea>
             </div>
             <div class="form-group">
-              <button class="btn" type="submit" name="send" type="submit">Kirim Pesan <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+              <button class="btn" type="submit" name="send" type="submit">Kirim Pesan</button>
             </div>
           </form>
         </div>
